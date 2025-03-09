c3b52a74b19d74c24f9a4a49d377961127008d4e
Colin Bradbury <129013355+DungeonMasterGaming@users.noreply.github.com>
2025-03-09 17:30:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+async function fetchData(sheet = 'Item Database') {
+  const url = 'https://script.google.com/macros/s/AKfycbyXDc0iMc2mWNxsCszUAADBHWG5WkBXrdI3b8WMMv86JBnea2kxK21_ns8ippDLNemdfQ/exec?sheet=' + encodeURIComponent(sheet);
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
 
+fetchData().then(data => {
+  console.log(data);
+  // Integrate data into your bookshelf UI here
+});
